0c8395c6ee7867f4f7c128aac34620f023f33289
DJ Doena <github@doena-soft.de>
2022-09-24 12:41:19 +0200

    1.1.2

--- EnhancedPurchaseInfoLibrary/Properties/AssemblyInfo.cs
+++ EnhancedPurchaseInfoLibrary/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Reflection;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Doena Soft.")]
 [assembly: AssemblyProduct("Enhanced Purchase Info Library")]
-[assembly: AssemblyCopyright("Copyright © Doena Soft. 2015 - 2021")]
+[assembly: AssemblyCopyright("Copyright © Doena Soft. 2015 - 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 
-[assembly: AssemblyVersion("1.1.1.4")]
-[assembly: AssemblyFileVersion("1.1.1.4")]
+[assembly: AssemblyVersion("1.1.2.0")]
+[assembly: AssemblyFileVersion("1.1.2.0")]
 
 [assembly: AssemblyDelaySign(false)]
